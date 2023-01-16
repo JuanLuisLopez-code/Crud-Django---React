b00b5ad316b043b76dd5b67f4d5c0555c5ca8197
JuanLuisLopez-code <juanluislopezdaw@gmail.com>
2023-01-16 15:23:05 +0100

    Lazy Load

--- frontend/react-hooks-crud/src/App.js
+++ frontend/react-hooks-crud/src/App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
-import CategoriasList from "./pages/CategoriasList";
-import CategoriasCreate from "./pages/CategoriasCreate";
-import CategoriasUpdate from "./pages/CategoriasUpdate";
+// import CategoriasList from "./pages/CategoriasList";
+// import CategoriasCreate from "./pages/CategoriasCreate";
+// import CategoriasUpdate from "./pages/CategoriasUpdate";
+import React from 'react';
 
 function App() {
+
+  const ListAll = React.lazy(() => import("./pages/CategoriasList"))
+  const CreateCategorias = React.lazy(() => import("./pages/CategoriasCreate"))
+  const UpdateCategorias = React.lazy(() => import("./pages/CategoriasUpdate"))
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<CategoriasList />} />
-          <Route path="/create" element={<CategoriasCreate />} />
+          <Route path="/" element={<ListAll />} />
+          <Route path="/create" element={<CreateCategorias />} />
           <Route path="/update/:id" element={<CategoriasUpdate />} />
-          <Route path="/categorias" element={<CategoriasList />} />
+          <Route path="/categorias" element={<UpdateCategorias />} />
         </Routes>
       </BrowserRouter>
     </div>
